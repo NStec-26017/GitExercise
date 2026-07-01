b14b4cd411a31efb3caed415c12a54093db4f71e
NStec-26017 <nstec26_017@fullness.email>
2026-07-01 10:29:31 +0900

    変更

--- app/src/main/java/numer0n/App.java
+++ app/src/main/java/numer0n/App.java
@@ -3,14 +3,15 @@
  */
 package numer0n;
 
-import numer0n.service.impl.GameManagerImpl;
+
+import numer0n.factory.service.GameManagerFactory;
 import numer0n.service.GameManager;
 
 public class App {
 
     public static void main(String[] args) {
         // ゲームを開始
-        GameManager gameManager = new GameManagerImpl();
+        GameManager gameManager = GameManagerFactory.create();
         gameManager.start();
 
     }
